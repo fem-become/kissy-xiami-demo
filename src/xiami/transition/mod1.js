e6be103a375a3d46fe2097a82300b5271971a086
chengyu <chengyu@taobao.com>
2013-04-11 23:04:42 +0800

    add common header

--- src/xiami/transition/mod1.js
+++ src/xiami/transition/mod1.js
@@ -1,25 +1,35 @@
-KISSY.add(function (S, Node, Transition,Event) {
+KISSY.add(function (S, Node, Transition,Event,header) {
     var $ = Node.all;
     var el;
+
+		var myName=this.getName();
+		
+		var body=$('#body');
 
     return {
 
         init: function () {
             if (!el) {
-                el = $('<div class="mod-page"><div class="mod-page-inner"></div></div>').appendTo('body');
+                el = $('<div class="mod-page"></div>').appendTo(body);
 
 
-                el.one('.mod-page-inner').html('mod1');
+                el.html('<button>go</button>');
 
-                el.on(Event.Gesture.tap, function () {
-                    Transition.forward('xiami/transition/mod1', 'xiami/transition/mod2');
+                el.one('button').on(Event.Gesture.tap, function () {
+                    Transition.forward(myName, 'xiami/transition/mod2');
                 });
 
-                S.log('mod1 is new');
+                S.log(myName+' is new');
             } else {
-                S.log('mod1 is coming again')
+                S.log(myName+' is coming again')
             }
-
+            
+            
+						var headerEl=header.getHeader(myName);
+						
+						if(!headerEl.contents().length){
+							headerEl.append(myName);
+						}
 
         },
 
@@ -30,5 +40,5 @@ KISSY.add(function (S, Node, Transition,Event) {
     };
 
 }, {
-    requires: ['node', './index','event']
+    requires: ['node', './index','event','../header']
 });
