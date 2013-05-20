580a678431cc91d532ecc00e3059cd8012af9bdb
bofang.zxj <bofang.zxj@taobao.com>
2013-05-20 12:05:20 +0800

    player

--- src/xiami/transition/player.js
+++ src/xiami/transition/player.js
@@ -1,6 +1,6 @@
 KISSY.add(function(S, Node, Transition, Event, header, DD, Constrain) {
 
-    var $ = Node.all;
+	var $ = Node.all;
 	var el;
 	var myName = this.getName();
 	var body = $("#body");
@@ -11,91 +11,234 @@ KISSY.add(function(S, Node, Transition, Event, header, DD, Constrain) {
 	var re = {};
 	var BASE_URL = 'http://test.fem.taobao.net:3000/song/';
 	var TEST_URL = 'http://m1.file.xiami.com/224/224/992/12345_95993_l.mp3';
-	var PLAY_CLASS = 'music-play';
-	var STOP_CLASS = 'music-pause';
+	var PLAY_CLASS = 'pl-play';
+	var STOP_CLASS = 'pl-pause';
 	var player = null;
 	var musicInfo = {};
 	var isPlaying = false;
 	var isStarted = false;
-	var PLAYER_TEMPLATE = '<div class="music-info-temp">' +
-		'	<h2 id="J_MusicTittle"></h2>' +
-		'	<img id="J_MusicImg" />' +
-		'	<p id="J_MusicLrc" ></p>' +
-		'</div>' +
-		'<p class="music-handle">' +
-		'	<a href="javascript:void(0);" id="J_HandleMusic" class="music-pause">' +
-		'		播放/停止' +
-		'	</a>' +
-		'</p>' +
-		'<div class="music-time">' +
-		'	<em id="J_Now" class="now">' +
-		'		0:00' +
-		'	</em>' +
-		'	<em id="J_Total" class="total">' +
-		'		0:00' +
-		'	</em>' +
-		'</div>' +
-		'<div id="J_BarBack" class="bar-background">' +
-		'	<div class="music-progress">' +
-		'		<div class="bar-content" id="J_BarContent">' +
-		'			<p id="J_ProgressBar" class="progress-bar"></p>' +
-		'			<em id="J_Drag" class="drag" draggable="true" title="滚动条"></em>' +
+	
+	
+	var elTabContent = [];
+
+
+	var LIST_TEMP_HTML = '<li class="J_MusicItem" id="J_MusicItem{id}" data-id="{id}">'+
+			'<p class="bar J_PlListBar" class="J_ItemBar"></p>'+
+			'<p class="pl-name">'+
+			'	<strong class="tt" title="{title}">'+
+			'		{title}'+
+			'	</strong>'+
+			'	<em>正在播放</em>'+
+			'</p>'+
+			//'<i class="J_TriggerMove"></i>'+
+		'</li>';
+	var HANDLE_TEMPLATE = '<div id="J_PlayInfo" class="pl-play-info">' +
+		'	<ul class="pl-tab-icon" id="J_TabHandle">' +
+		'		<li id="J_TabImg" data-index="0" class="J_PlayTabIcon pl-hover">' +
+		'		</li>' +
+		'		<li id="J_TabList" data-index="1" class="J_PlayTabIcon">' +
+		'		</li>' +
+		'	</ul>' +
+		'	<p id="J_PlayTime" class="pl-play-time">' +
+		'		<em id="J_CurrentTime" class="pl-current">' +
+		'			00:00' +
+		'		</em>' +
+		'		<em id="J_TotalTime" class="pl-total">' +
+		'			00:00' +
+		'		</em>' +
+		'	</p>' +
+		'	<div id="J_PlayBar" class="pl-play-bar">' +
+		'		<p id="J_PlayProgress" class="pl-progress"></p>' +
+		'		<em id="J_PlayBarIcon" class="pl-bar-icon"></em>' +
+		'	</div>' +
+		'	<div class="pl-player-handle">' +
+		'		<span id="J_PlayLast" class="pl-last">' +
+		'			<em>last</em>' +
+		'		</span>' +
+		'		<span id="J_PlaySwitch" class="pl-switch pl-pause">' +
+		'			<em>play/pause</em>' +
+		'		</span>' +
+		'		<span id="J_PlayNext" class="pl-next">' +
+		'			<em>next</em>' +
+		'		</span>' +
+		'	</div>' +
+		'</div>';
+
+
+	var TAB_CONTENT_TEMP = '<div id="J_PlTabContent" class="pl-tab-content">' +
+		'	<div id="J_PlayerTab" class="pl-player-tab">' +
+		'		<div class="pl-img-tab" id="J_PlImgTab">' +
+		'			<div class="pl-img-content">' +
+		'				<img class="pl-img" id="J_PlImg" src="" alt="img">' +
+		'			</div>	' +
+		'		</div>' +
+		'		<div class="pl-list-tab" id="J_PlListTab" style="display:none">' +
+		'			<ul class="pl-music-list" id="J_PlMusicList">' +
+		'			</ul>' +
 		'		</div>' +
 		'	</div>' +
 		'</div>';
 
 
-
+	//播放列表
+	var listArr = localStorage.getItem('MUSIC_LIST').split(',');
 	S.mix(re, {
 
 		init: function(config) {
-			S.log("playing music: " + config.id);
-			musicInfo = {
-				id: config.id
-			};
-			this.render();
+			if(!config || !config.id){
+				musicInfo.id = listArr[0];
+			}else{
+				musicInfo.id = config.id;
+			}
+			
+			
 			if (!el) {
 				el = $('<div class="mod-page"></div>').appendTo(body);
-				el.html(PLAYER_TEMPLATE);
+				el.html(TAB_CONTENT_TEMP + HANDLE_TEMPLATE);
 				S.log(myName + ' is new');
 			} else {
 				S.log(myName + ' is coming again');
 			}
+
+
+			this.render();
+			this.fillList();
+			this._bindEvent();
+
 
 			var headerEl = header.getHeader(myName);
 			if (!headerEl.contents().length) {
 				headerEl.append(myName);
 			}
 		},
-
 		getEl: function() {
 			return el;
 		},
-
 		/**
 		 * 渲染
 		 * @return {Object} this
 		 */
-		render: function() {
+		_bindEvent: function() {
+			this._pointMusic();
+			this.playNext();
+			this.playPrev();
+			this._bindTabSwitch();
+			var self = this;
+
+		},
+		/**
+		 * 填充列表
+		 * @return {Object} this
+		 */
+		fillList: function() {
+			// var test = [12345, 11024, 11020, 11026, 12024];
+			// localStorage.setItem('MUSIC_LIST', test.toString());
+			var listA = [];
+			//var listArr = localStorage.getItem('MUSIC_LIST').split(',');
+			//alert(listArr)
+			S.each(listArr, function(value, key) {
+				var self = this;
+				var shopInfo = {
+					id: value
+				};
+				S.io({
+					type: "get",
+					url: BASE_URL + value,
+					dataType: "jsonp",
+					success: function(data) {
+						shopInfo = S.mix(shopInfo, data);
+						listA.push(S.substitute(LIST_TEMP_HTML, shopInfo));
+						if (key === listArr.length - 1) {
+							S.log(listA)
+							$('#J_PlMusicList').html(listA.join(''));
+							re._changeColor();
+						}
+					},
+					error: function() {
+						S.log('error');
+					}
+				});
+			});
+			return this;
+		},
+		_bindTabSwitch: function() {
+			var self = this;
+			Event.delegate(document, 'click', '.J_PlayTabIcon', function(e) {
+				var elTarget = $(e.target);
+				var index = parseInt(elTarget.attr('data-index'), 10);
+				self.switchTab(index);
+			});
+			$('#J_PlImgTab').on('swipe', function(e) {
+				if (e.direction === 'left') {
+					self.switchTab(1);
+				}
+			});
+			$('#J_PlListTab').on('swipe', function(e) {
+				if (e.direction === 'right') {
+					self.switchTab(0);
+				}
+			});
+		},
+		_changeColor: function() {
+			//alert($('.J_MusicItem').length);
+			$('.J_MusicItem').each(function(v, index) {
+				//alert(index)
+				if (index % 2 === 1) {
+					v.addClass('odd');
+				}
+			})
+		},
+		/**
+		 * tab 切换
+		 * @param  {Number} index
+		 * @return {Object}      this
+		 */
+		switchTab: function(index) {
+			var width = $(window).width();
+			var elTab = $('#J_PlayerTab');
+			var elTabArr = [$('#J_PlImgTab'), $('#J_PlListTab')];
+			var elTabIconArr = [$('#J_TabImg'),$('#J_TabList')];
+			var lenArr = [0, width];
+
+			elTabArr[index].show();
+			elTabArr[0].animate({
+				'left': '-' + lenArr[index]
+			}, 0.5, undefined, function() {
+				//elImg.hide();
+			});
+			elTabArr[1].animate({
+				'left': lenArr[1 - index]
+			}, 0.5, undefined, function() {
+				//S.log(elTabIconArr);
+				elTabArr[1 - index].hide();
+				elTabIconArr[index].addClass('pl-hover');
+				elTabIconArr[1 - index].removeClass('pl-hover');
+			});
+			return this;
+		},
+		/**
+		 * 渲染
+		 * @return {Object} this
+		 */
+		render: function(isSwitch) {
 			var self = this;
 			// reset
 			player = null;
 			progress = null;
+			//alert(1)
 			this.getMusicInfo(musicInfo.id, function() {
+				//alert('aaaaaaaaa')
+				//alert('get')
+				//alert(1)
 				//接口暂时有问题
-				console.log(musicInfo.location);
-				self.createAudio(musicInfo.location);
-				// self.createAudio(TEST_URL);
-				$('#J_MusicTittle').html(musicInfo.title);
-				$('#J_MusicImg').html(musicInfo.title);
-				$('#J_MusicLrc').html(musicInfo.lrc);
-				
-					self.play(function() {
-						isPlaying = true;
-						isStarted = true;
-						self.updateProgress();
-						$('#J_HandleMusic').replaceClass(STOP_CLASS, PLAY_CLASS);
-					});
+				//console.log(musicInfo)
+				self.createAudio(musicInfo.location,isSwitch);
+
+				//self.createAudio(TEST_URL, isSwitch);
+
+				//$('#J_MusicTittle').html(musicInfo.title);
+				$('#J_PlImg').attr('src', musicInfo.albumCover);
+				//$('#J_MusicLrc').html(musicInfo.lrc);
 			});
 
 			return this;
@@ -104,17 +247,23 @@ KISSY.add(function(S, Node, Transition, Event, header, DD, Constrain) {
 		 * 创建Audio
 		 * @return {Object} player
 		 */
-		createAudio: function(src) {
+		createAudio: function(src, isSwitch) {
 			player = new Audio();
 			player.src = src;
 			var self = this;
-			// 音乐资源加载完毕
-			$(player).on('loadedmetadata', function() {
+			player.load();
+			//alert('creat');
+			//alert(1)
+			$(player).on('canplay', function() {
+				//alert(1);
 				self._changeProgress();
 				self._switchMusic();
-				self._endMusic();
+				//self._endMusic();
 				self._setProgress();
-				self._dragProgress();
+				//self._dragProgress();
+				if (isSwitch) {
+					self.play();
+				}
 			});
 			return player;
 		},
@@ -157,7 +306,10 @@ KISSY.add(function(S, Node, Transition, Event, header, DD, Constrain) {
 		 * @return {Object} this
 		 */
 		bringRest: function() {
-			player.progress(0);
+			player && player.pause();
+			player.currentTime && (player.currentTime = 0);
+			progress && progress.cancel();
+			this.progress(0);
 			return this;
 		},
 		/**
@@ -167,16 +319,22 @@ KISSY.add(function(S, Node, Transition, Event, header, DD, Constrain) {
 		 * @return {Object} this
 		 */
 		getMusicInfo: function(id, callback) {
+			//callback();					
 			S.io({
 				type: "get",
 				url: BASE_URL + musicInfo.id,
 				dataType: "jsonp",
 				success: function(data) {
+					//alert('success')
 					S.mix(musicInfo, data);
 					//musicInfo = data;
-					S.log('Music Info :');
-					S.log(musicInfo);
+					console.log('Music Info :');
+					console.log(musicInfo);
 					callback();
+				},
+				error: function() {
+					alert('error')
+					callback && callback();
 				}
 			});
 			return this;
@@ -195,17 +353,28 @@ KISSY.add(function(S, Node, Transition, Event, header, DD, Constrain) {
 			}, realRate, true);
 			return this;
 		},
+		/**
+		 * 获取窗口长度，考虑到用pc打开的时候，可能会调整窗口大小，所以这个值需要动态获取
+		 * @return {[type]} [description]
+		 */
+		getWindowWidth: function() {
+			return $(window).width();
+		},
 		_changeProgress: function() {
-			var elBar = $('#J_ProgressBar'),
-				elNow = $('#J_Now'),
-				elTotal = $('#J_Total'),
-				elDrag = $('#J_Drag');
+			var elBar = $('#J_PlayProgress'),
+				elNow = $('#J_CurrentTime'),
+				elTotal = $('#J_TotalTime'),
+				elDrag = $('#J_PlayBarIcon');
+			var elListBar = $('#J_MusicItem' + musicInfo.id).children('.J_PlListBar');
 			// 当前播放的时间 （值可set）
 			var nowTime = this.progress(),
 				// 歌曲的总时间
 				totalTime = this.getTotalTime();
-			var len = (nowTime / totalTime) * 210;
+			var len = (nowTime / totalTime) * this.getWindowWidth();
 			elBar.css({
+				width: len
+			});
+			elListBar.css({
 				width: len
 			});
 			elDrag.css({
@@ -245,33 +414,66 @@ KISSY.add(function(S, Node, Transition, Event, header, DD, Constrain) {
 		_switchMusic: function() {
 			var self = this,
 				elTarget;
-			$('#J_HandleMusic').on('click', function() {
+			$('#J_PlaySwitch').detach('click').on('click', function() {
+				//alert(1)
 				elTarget = $(this);
-				if (elTarget.hasClass('music-pause')) {
-					self.play(function() {
-						isPlaying = true;
-						isStarted = true;
-						self.updateProgress();
-						elTarget.replaceClass(STOP_CLASS, PLAY_CLASS);
-					});
+				if (elTarget.hasClass(STOP_CLASS)) {
+					self._startPlay();
 				} else {
 					self.stop(function() {
 						isPlaying = false;
-						progress.cancel();
+						progress && progress.cancel();
 						elTarget.replaceClass(PLAY_CLASS, STOP_CLASS);
 					});
 				}
 			});
 		},
 		/**
+		 * 下一首
+		 * @return {Object} this
+		 */
+		playNext: function() {
+			var elTarget = $('#J_PlayNext');
+			var nextNode;
+			elTarget.on('click', function() {
+				nextNode = $('.is-playing').next('.J_MusicItem');
+				nextNode && nextNode.fire(Event.Gesture.doubleTap);
+			});
+			return this;
+		},
+		/**
+		 * 上一首
+		 * @return {Object} this
+		 */
+		playPrev: function() {
+			var elTarget = $('#J_PlayLast');
+			var prevNode;
+			elTarget.on('click', function() {
+				prevNode = $('.is-playing').prev('.J_MusicItem');
+				prevNode && prevNode.fire(Event.Gesture.doubleTap);
+			});
+			return this;
+		},
+		/**
+		 * 开始播放，用于调整进度条
+		 */
+		_startPlay: function() {
+			var self = this;
+			var elTarget = $('#J_PlaySwitch');
+			self.play(function() {
+				isPlaying = true;
+				isStarted = true;
+				self.updateProgress();
+				elTarget.replaceClass(STOP_CLASS, PLAY_CLASS);
+			});
+		},
+		/**
 		 * 结束播放
-		 * @return {[type]} [description]
 		 */
 		_endMusic: function() {
+			var self = this;
 			$(player).on('ended', function() {
-				S.log('over');
-				isPlaying = false;
-				isStarted = false;
+				$('#J_PlayNext').fire('click');
 			});
 		},
 		/**
@@ -279,10 +481,10 @@ KISSY.add(function(S, Node, Transition, Event, header, DD, Constrain) {
 		 */
 		_setProgress: function() {
 			var self = this;
-			$('#J_BarContent').on('click', function(e) {
+			$('#J_PlayBar').on('click', function(e) {
 				//if (!isStarted) return;
 				//player.currentTime = (e.offsetX / 210) * self.getTotalTime();
-				self.progress((e.offsetX / 210) * self.getTotalTime());
+				self.progress((e.offsetX / self.getWindowWidth()) * self.getTotalTime());
 				self._changeProgress();
 			});
 		},
@@ -297,7 +499,7 @@ KISSY.add(function(S, Node, Transition, Event, header, DD, Constrain) {
 				var left = elDrag.css('left');
 				//if (left > 207) left = 207;
 				elDrag.css('left', left);
-				self.progress(((parseFloat(left, 10) + 3) / 210) * self.getTotalTime());
+				self.progress(((parseFloat(left, 10) + 3) / self.getWindowWidth()) * self.getTotalTime());
 				self._changeProgress();
 			});
 		},
@@ -315,9 +517,27 @@ KISSY.add(function(S, Node, Transition, Event, header, DD, Constrain) {
 					constrain: '#J_BarBack' // 限制拖动区域为视窗区域
 				})]
 			});
+		},
+		_pointMusic: function() {
+			var self = this;
+			//alert(KISSY.Event.Gesture.doubleTap)
+			Event.delegate(document, Event.Gesture.doubleTap, '.J_MusicItem', function(e) {
+				var elTarget = $(e.target);
+				//移动标签不用绑定事件
+				if (elTarget.hasClass('J_TriggerMove')) return;
+
+				musicInfo.id = elTarget.attr('data-id');
+				elTarget.addClass('is-playing').siblings('.J_MusicItem').removeClass('is-playing');
+				//console.log(musicInfo.id);
+				self.bringRest();
+				progress && progress.cancel();
+				self.render(true);
+				self._startPlay();
+				$('#J_PlaySwitch').replaceClass(STOP_CLASS, PLAY_CLASS);
+
+			});
 		}
 	});
-
 	return re;
 
 }, {
